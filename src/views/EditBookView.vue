43520871e262b0a630551e7f46668f68c90f277c
Katharina Endner <k.endner@gmail.com>
2023-05-31 09:29:02 +0200

    feat: adds input fields für editing books

--- src/views/EditBookView.vue
+++ src/views/EditBookView.vue
@@ -4,5 +4,51 @@
       >Back</router-link
     >
   </button>
-  <div>test</div>
+
+  <h1>Book details</h1>
+  <div>
+    Title:
+    <input type="text" name="title" v-model="book.title" />
+  </div>
+  <div>
+    Author:
+    <input type="text" name="author" v-model="book.author" />
+  </div>
+  <div>
+    Abstract:
+    <input type="text" name="abstract" id="abstract" v-model="book.abstract" />
+  </div>
+  <div>Price:<input type="text" name="price" v-model="book.price" /></div>
+  <div>ISBN:<input type="text" name="ISBN" v-model="book.isbn" /></div>
+
+  <button @click="saveChanges">Save Changes</button>
 </template>
+
+<script>
+export default {
+  name: "EditBookView",
+  data() {
+    return {
+      book: {},
+    };
+  },
+  created() {
+    fetch("http://localhost:4730/books/" + this.$route.params.isbn)
+      .then((res) => res.json())
+      .then((book) => (this.book = book));
+  },
+  methods: {
+    saveChanges() {},
+  },
+};
+</script>
+
+<style scoped>
+input {
+  min-width: 20rem;
+}
+
+#abstract {
+  min-height: 5rem;
+}
+</style>
